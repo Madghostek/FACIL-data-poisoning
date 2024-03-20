aebfb8d2c1e862d1ad5b6ebf7a97a9ef311f1a39
Tomasz S <madghostek@gmail.com>
2024-03-20 23:14:12 +0100

    implement ratio in command line for all methods

--- experiments/utils/manage_dataset.py
+++ experiments/utils/manage_dataset.py
@@ -46,17 +46,36 @@ def make_dataset_skeleton(path):
 
 	return train,test
 
-def create_poisoned_cifar_square(path=dataset_path, target_classes=(3,7), ratio=1.0,*, pattern_strength=1.0,):
-	logger = logging.getLogger(__name__)
+def get_amount_to_modify(train,test,target_classes,ratio):
+	counter_train = {c: 0 for c in target_classes}
+	counter_test = {c: 0 for c in target_classes}
+	
+	# get count of target classes in train and test, should be 500 and 100 respectively:
+	for counter,dataset in zip((counter_train,counter_test),(train,test)):
+		for cls in dataset.targets:
+			if cls in counter:
+				counter[cls]+=1
+	
+	# get the final amount of modified elements 
+	for counter in (counter_train,counter_test):
+		for k in counter:
+			counter[k]=int(counter[k]*ratio)
 
+	return counter_train,counter_test
+
+def create_poisoned_cifar_square(path=dataset_path, target_classes=(3,7), ratio=1.0,*, pattern_strength=1.0):
+	logger = logging.getLogger(__name__)
 	train,test = make_dataset_skeleton(path)
 
+	counter_train,counter_test = get_amount_to_modify(train,test,target_classes, ratio)
+	logger.debug(f"counts: {counter_train},{counter_test}")
+
 	with open(path+"/test.txt","w+") as test_fp,open(path+"/train.txt","w+") as train_fp:
-		for mode,data,targets in (("train",train.data,train.targets),("test",test.data,test.targets)):
-			logger.info(f"transforming {mode} images")
+		for mode,data,targets,counter in (("train",train.data,train.targets,counter_train),("test",test.data,test.targets,counter_test)):
+			logger.info(f"transforming {mode} images, {sum(counter)} in total")
 			for idx,(image,cl) in enumerate(tqdm(zip(data,targets),total=len(data))):
 				# transform image
-				if cl in target_classes:
+				if cl in counter and counter[cl]>0:
 					if DEBUG:
 						print(mode,image,cl)
 						plt.imshow(image)
@@ -66,6 +85,10 @@ def create_poisoned_cifar_square(path=dataset_path, target_classes=(3,7), ratio=
 						print(mode,image,cl)
 						plt.imshow(image)
 						plt.show()
+					counter[cl]-=1
+					if sum(counter.values())==0:
+						logger.debug("Found all samples, breaking...")
+						break
 
 				# save as image in correct folder and name
 				im = Image.fromarray(image)
@@ -89,12 +112,15 @@ def create_poisoned_cifar_blend_one_image(path=dataset_path, target_classes=(3,7
 	logger = logging.getLogger(__name__)
 	train,test = make_dataset_skeleton(path)
 
+	counter_train,counter_test = get_amount_to_modify(train,test,target_classes, ratio)
+	logger.debug(f"counts: {counter_train},{counter_test}")
+
 	# take some totally random image
 	to_blend = train.data[0]
 
 	with open(path+"/test.txt","w+") as test_fp,open(path+"/train.txt","w+") as train_fp:
-		for mode,data,targets in (("train",train.data,train.targets),("test",test.data,test.targets)):
-			logger.info(f"transforming {mode} images")
+		for mode,data,targets,counter in (("train",train.data,train.targets,counter_train),("test",test.data,test.targets,counter_test)):
+			logger.info(f"transforming {mode} images, {sum(counter)} in total")
 			for idx,(image,cl) in enumerate(tqdm(zip(data,targets),total=len(data))):
 				# transform image
 				if cl in target_classes:
@@ -107,6 +133,10 @@ def create_poisoned_cifar_blend_one_image(path=dataset_path, target_classes=(3,7
 						print(mode,image,cl)
 						plt.imshow(image)
 						plt.show()
+					counter[cl]-=1
+					if sum(counter.values())==0:
+						logger.debug("Found all samples, breaking...")
+						break
 
 				# save as image in correct folder and name
 				im = Image.fromarray(image)
@@ -116,6 +146,14 @@ def create_poisoned_cifar_blend_one_image(path=dataset_path, target_classes=(3,7
 				#append class and path to file
 				fp = train_fp if mode=="train" else test_fp
 				fp.write(f"{rel_path} {cl}\n") #path and class
+
+	meta = {"poisonType":"white-square",
+		"targetClasses":target_classes,
+		"ratio":ratio,
+		"blend_amount":blend_amount}
+
+	with open(path+"/"+meta_fname,"w+") as f:
+		json.dump(meta,f)
 
 #--- poison methods end
 
@@ -131,6 +169,7 @@ def get_current_dataset(path=dataset_path):
 		return None
 
 def remove_dataset(path=dataset_path):
+		# don't care about exceptions (can't put it in single surpress...)
 		logger = logging.getLogger(__name__)
 		logger.info(f"Removing dataset at {path}")
 		with suppress(FileNotFoundError):
@@ -159,6 +198,13 @@ def main():
         choices=['white-square', 'blend-one-image', 'blend-random'],
         help='Dataset to create: white-square, blend, option3 (default: white-square)',
         default=None
+    )
+
+	parser.add_argument(
+        '--ratio',
+        help='Value between 0 and 1, how many images to transform.',
+        type=float,
+		default=1.0
     )
 
 	parser.add_argument(
@@ -197,9 +243,14 @@ def main():
 
 
 	if args.poison_method=="white-square":
-		create_poisoned_cifar_square()
-	if args.poison_method=="blend-one-image":
-		create_poisoned_cifar_blend_one_image()
+		create_poisoned_cifar_square(ratio=args.ratio)
+	elif args.poison_method=="blend-one-image":
+		create_poisoned_cifar_blend_one_image(ratio=args.ratio)
+	elif args.poison_method=="blend-random":
+		raise NotImplementedError("TODO")
+	else:
+		raise ValueError("Invalid poison method")
+	
 
 if __name__=="__main__":
 	main()
