1325f638d09652009cd9dd754813242bf06032c1
Tomasz S <madghostek@gmail.com>
2024-04-03 23:24:02 +0200

    add seed

--- experiments/train_poisoned_cifar.sh
+++ experiments/train_poisoned_cifar.sh
@@ -1,3 +1,3 @@
 cd ..
 echo training with $1 buffer size and $2 epochs
-python3 -u src/main_incremental.py --approach joint --dataset cifar_10_poisoned --num-tasks 5 --nepochs $2 --num-exemplars $1 
+python3 -u src/main_incremental.py --seed 1234 --dataset cifar_10_poisoned --num-tasks 5 --nepochs $2 --num-exemplars $1 
