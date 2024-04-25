c311f80b25395eacb8289e2659b58900b3948e3c
Tomasz S <madghostek@gmail.com>
2024-04-25 03:12:13 +0200

    use seed properly, use correct dataset...

--- experiments/train_lwf.sh
+++ experiments/train_lwf.sh
@@ -1,3 +1,3 @@
 cd ..
 echo training with $1 epoch $2 lamb $3 seed
-python3 -u src/main_incremental.py --approach lwf --seed $3 --dataset cifar10 --num-tasks 5 --nepochs $1 --lamb $2
+python3 -u src/main_incremental.py --approach lwf --seed $3 --dataset cifar_10_poisoned --num-tasks 5 --nepochs $1 --lamb $2
